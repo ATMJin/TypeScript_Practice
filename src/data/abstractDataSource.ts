f8529772d1bfd2d341780aafe9bffe1b5e9f701e
ATMJin <atmjin0912@protonmail.com>
2022-06-07 09:04:56 +0800

    建立資料來源

---
+++ src/data/abstractDataSource.ts
@@ -0,0 +1,56 @@
+import { Product, Order } from './entities';
+
+export type ProductProp = keyof Product;
+
+// 資料來源的抽象類別
+export abstract class AbstractDataSource {
+  private _products: Product[];  // 產品清單
+  private _categeories: Set<string>;  // 產品分類 (Set,元素為字串)
+  public order: Order;  // 購物車
+  public loading: Promise<void>;  // 資料來源是否讀取完畢
+
+  constructor() {
+    this._products = [];
+    this._categeories = new Set<string>();
+    this.order = new Order();
+    this.loading = this.getData();
+  }
+
+  // 從來源讀入產品與分類資料
+  protected async getData(): Promise<void> {
+    this._products = [];
+    this._categeories.clear();
+    const rawData = await this.loadProducts();
+    rawData.forEach(p => {
+      this._products.push(p);
+      this._categeories.add(p.category);
+    });
+  }
+  // 傳回產品列表 (呼叫 selectProducts(), 根據產品id和指定分類排序)
+  async getProducts(sortProp: ProductProp = "id", category?: string): Promise<Product[]> {
+    await this.loading;
+    return this.selectProducts(this._products, sortProp, category);
+  }
+
+  // 根據指定的產品屬性排序，傳回符合指定分類的產品列表
+  protected selectProducts(prods: Product[],
+    sortProp: ProductProp,
+    category?: string): Product[] {
+    return prods.filter(p => category === undefined || p.category === category)
+      .sort((p1, p2) => p1[sortProp] < p2[sortProp] ?
+        -1 : p1[sortProp] > p2[sortProp] ?
+          1 : 0);
+  }
+
+  // 傳回產品分類
+  async getCategories(): Promise<string[]> {
+    await this.loading;
+    return [...this._categeories.values()];
+  }
+
+  // 得由子類別實作的方法
+  // 載入產品資料
+  protected abstract loadProducts(): Promise<Product[]>;
+  // 儲存訂單
+  abstract storeOrder(): Promise<number>;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-let hellow:string="Hellow World!!"
-console.log(hellow);
+import { LocalDataSource } from "./data/localDataSource";
+
+// 非同步的產品資料展示函式 (傳回一個很長的字串)
+async function displayData(): Promise<string> {
+  // 載入商品與分類資料當成 data store
+  let ds = new LocalDataSource();
+  // 取得產品物件
+  let allProducts = await ds.getProducts("name");
+  // 取得產品分類
+  let categories = await ds.getCategories();
+
+  // 取出西洋棋商品
+  let chessProducts = await ds.getProducts("name", "Chess");
+  let result = "";
+
+  // 在字串加入每種產品的名稱和分類
+  allProducts.forEach(p => result += `Product: ${p.name}, ${p.category}\n`);
+  // 在字串加入所有分類
+  categories.forEach(c => result += (`Category: ${c}\n`));
+  // 將所有西洋棋產品放入購物車
+  chessProducts.forEach(p => ds.order.addProduct(p, 1));
+  // 在字串加入購物車總金額
+  result += `Order total: $${ds.order.total.toFixed(2)}`;
+
+  return result;
+}
+
+// 在Promise有傳回值時印出來
+displayData().then(res => console.log(res));
